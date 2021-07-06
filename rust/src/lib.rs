6bb52e79835fc81cc7f125ccfe8a9b965837a083
l1npengtul <l1npengtul@protonmail.com>
2021-07-06 12:53:21 +0900

    Initial Commit

---
+++ rust/src/lib.rs
@@ -0,0 +1,12 @@
+mod game;
+mod character_trait;
+
+use gdnative::prelude::{godot_init, InitHandle};
+
+// Function that registers all exposed classes to Godot
+fn init(handle: InitHandle) {
+    handle.add_class::<game::Game>();
+}
+
+// macros that create the entry-points of the dynamic library.
+godot_init!(init);
